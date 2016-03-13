f654f5543806882f1bc06f1d6457448eaf86b122
shmikel_home <shmikel0012@gmail.com>
2016-03-13 12:16:52 +0300

    Try again

---
+++ BookOrganizer/ViewModels/AddBookViewModel.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+
+using BookOrganizer.Commands;
+using System.Linq;
+
+namespace BookOrganizer.ViewModels
+{
+    public class AddBookViewModel : ViewModelBase
+    {
+        #region Book object with public methods
+        private Book book;
+        private List<Genre> genres;
+
+        public List<Genre> Genres
+        {
+            get { return genres; }
+            set { genres = value; OnPropertyChanged("Genres"); }
+        }
+        public string Author
+        {
+            get { return book.Author; }
+            set
+            {
+                book.Author = value;
+                OnPropertyChanged("Author");
+            }
+        }
+        public string Title
+        {
+            get { return book.Title; }
+            set
+            {
+                book.Title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+        public int Year
+        {
+            get { return book.Year; }
+            set
+            {
+                book.Year = value;
+                OnPropertyChanged("Year");
+            }
+        }
+        public string Annotation
+        {
+            get { return book.Annotation; }
+            set
+            {
+                book.Annotation = value;
+                OnPropertyChanged("Annotation");
+            }
+        }
+        public string Comment
+        {
+            get { return book.Comment; }
+            set
+            {
+                book.Comment = value;
+                OnPropertyChanged("Comment");
+            }
+        }
+        public DateTime? FinishTime
+        {
+            get { return book.FinishTime; }
+            set
+            {
+                book.FinishTime = value;
+                OnPropertyChanged("FinishTime");
+            }
+        }
+        public DateTime? StartTime
+        {
+            get { return book.StartTime; }
+            set
+            {
+                book.StartTime = value;
+                OnPropertyChanged("StartTime");
+            }
+        }
+        public Genre Genre
+        {
+            get { return book.Genre; }
+            set
+            {
+                book.Genre = value;
+                OnPropertyChanged("Genre");
+            }
+        }
+        public int Id
+        {
+            get { return book.Id; }
+            set
+            {
+                book.Id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        public int Mark
+        {
+            get { return book.Mark; }
+            set
+            {
+                book.Mark = value;
+                OnPropertyChanged("Mark");
+            }
+        }
+        public int Pages
+        {
+            get { return book.Pages; }
+            set
+            {
+                book.Pages = value;
+                OnPropertyChanged("Pages");
+            }
+        }
+        #endregion
+        #region visual properties
+        private int selectedMode = 0;
+        public int SelectedMode
+        {
+            get { return selectedMode; }
+            set
+            {
+                selectedMode = value;
+                switch (value)
+                {
+                    case 0:
+                        AfterRead = true; break;
+                    case 1:
+                        AfterRead = false; break;
+                    case 2:
+                        AfterRead = false; break;
+                }
+                OnPropertyChanged("SelectedMode");
+            }
+        }
+        public int[] AvailableMarks
+        {
+            get
+            { return new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; }
+        }
+        private bool afterRead = true;
+        public bool AfterRead
+        {
+            get { return afterRead; }
+            set
+            {
+                afterRead = value; OnPropertyChanged("AfterRead");
+            }
+        }
+        #endregion
+        public Action<Book> BookOut;
+        #region Constructor
+
+        public AddBookViewModel(Book book)
+        {
+            this.book = book;
+            //hello, logic of changing existing item
+        }
+        public AddBookViewModel(string author = "", string title = "", string year = "0", int pages = 0)
+        {
+            using (var c = new Context())
+            {
+                Genres = c.Genres.ToList();
+            }
+            book = new Book() { Title = title, Author = author, Year = int.Parse(year), Pages = pages };
+        }
+
+        #endregion
+
+        #region SubmitCommand
+        private DelegateCommand submitCommand;
+        public ICommand SubmitCommand
+        {
+            get
+            {
+                if (submitCommand == null)
+                {
+                    submitCommand = new DelegateCommand(Submit);
+                }
+                return submitCommand;
+            }
+        }
+
+        private void Submit()
+        {
+            if (BookOut != null) { BookOut(book); }
+        }
+
+        #endregion
+    }
+}
---
+++ BookOrganizer/Views/AddBookView.xaml.cs
@@ -0,0 +1,72 @@
+﻿using BookOrganizer.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BookOrganizer.Views
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class AddBookView : Window
+    {
+        public AddBookView()
+        {
+            InitializeComponent();
+
+        }
+
+        private void OnlyDigitsOnlyFour(object sender, KeyEventArgs e)
+        {
+            string[] alow = new string[] { "D0", "D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9" };
+            foreach (var item in alow)
+            {
+                if (item == e.Key.ToString())
+                {
+                    switch ((((TextBox)sender).Text.Trim()).Count())
+                    {
+                        case 3:
+                            int y = DateTime.Now.Year;
+                            int pressed = int.Parse(e.Key.ToString().Substring(1));
+                            if (int.Parse(((TextBox)sender).Text.Trim() + pressed.ToString()) > y) { ((TextBox)sender).Text = y.ToString(); } else { return; }
+                            break;
+                        case 4: e.Handled = true; break;
+                        default: return;
+                    }
+                }
+            }
+            e.Handled = true;
+        }
+
+        private void FinishTimeBox_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (StartTimeBox.SelectedDate == null) { FinishTimeBox.SelectedDate = null; return; }
+            var dS = Convert.ToDateTime(StartTimeBox.SelectedDate);
+            var dF = Convert.ToDateTime(FinishTimeBox.SelectedDate);
+            if (dF.CompareTo(dS) == -1) { FinishTimeBox.SelectedDate = StartTimeBox.SelectedDate; }
+        }
+
+        private void PagesBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            string[] alow = new string[] { "D0", "D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9" };
+            foreach (var item in alow)
+            {
+                if (item == e.Key.ToString())
+                {
+                    return;
+                }
+            }
+            e.Handled = true;
+        }
+    }
+}
